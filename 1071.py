9033708c347a854d741d4a17200c41d27f15a8e0
JeanSchuambach <jeanschuambach@id.uff.br>
2016-07-30 12:16:33 -0300

    v

--- 1071.py
+++ 1071.py
@@ -1,11 +1,14 @@
 X = int(input())
 Y = int(input())
+Soma = 0
 
 if X > Y:
-    
+    for i in range(Y+1, X):
+        if not (i%2==0):
+            Soma = i + Soma
 else:
-    
-    
-print(Soma, end='\n')
+    for a in range(X+1, Y):
+        if not (a % 2 == 0):
+            Soma = a + Soma
 
-=
+print(Soma, end='\n')
